b4061c728a98be8ba94f3cfc0292fe6bf1d7a8a5
ssilatel <setaxe25@gmail.com>
2023-08-29 11:23:36 +0200

    Implemented loading ROMs

--- src/chip8.c
+++ src/chip8.c
@@ -4,11 +4,16 @@
 #include <SDL2/SDL.h>
 #include "chip8.h"
 
+SDL_Window*   window;
+SDL_Renderer* renderer;
+
 void initChip8();
+int  loadROM(const char* filepath);
 
 int main(int argc, char** argv)
 {
     initChip8();
+    loadROM("roms/ibm-logo.ch8");
     return 0;
 }
 
@@ -25,4 +30,21 @@ void initChip8()
     sp = 0;
     memset(keyboard, 0, 16);
     memcpy(memory + 0x50, fonts, 80 * sizeof(uint8_t));
+}
+
+int loadROM(const char* filepath)
+{
+    FILE* infile = fopen(filepath, "rb");
+    if (infile == NULL)
+    {
+        fprintf(stderr, "Couldn't open ROM: %s\n", filepath);
+        return 1;
+    }
+
+    fseek(infile, 0, SEEK_END);
+    int size = ftell(infile);
+    fseek(infile, 0, SEEK_SET);
+
+    fread(memory + 0x200, sizeof(uint16_t), size, infile);
+    return 0;
 }

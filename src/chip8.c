b95493604391485652acd1fc8034de4f0697a45c
ssilatel <setaxe25@gmail.com>
2023-08-30 10:37:29 +0200

    Adding keyboard mapping

--- src/chip8.c
+++ src/chip8.c
@@ -62,7 +62,109 @@ int main(int argc, char** argv)
                         case SDLK_ESCAPE:
                             running = 0;
                             break;
+                        case SDLK_1:
+                            keyboard[0x1] = 1;
+                            break;
+                        case SDLK_2:
+                            keyboard[0x2] = 1;
+                            break;
+                        case SDLK_3:
+                            keyboard[0x3] = 1;
+                            break;
+                        case SDLK_4:
+                            keyboard[0xC] = 1;
+                            break;
+                        case SDLK_q:
+                            keyboard[0x4] = 1;
+                            break;
+                        case SDLK_w:
+                            keyboard[0x5] = 1;
+                            break;
+                        case SDLK_e:
+                            keyboard[0x6] = 1;
+                            break;
+                        case SDLK_r:
+                            keyboard[0xD] = 1;
+                            break;
+                        case SDKL_a:
+                            keyboard[0x7] = 1;
+                            break;
+                        case SDLK_s:
+                            keyboard[0x8] = 1;
+                            break;
+                        case SDLK_d:
+                            keyboard[0x9] = 1;
+                            break;
+                        case SDLK_f:
+                            keyboard[0xE] = 1;
+                            break;
+                        case SDLK_z:
+                            keyboard[0xA] = 1;
+                            break;
+                        case SDLK_x:
+                            keyboard[0x0] = 1;
+                            break;
+                        case SDLK_c:
+                            keyboard[0xB] = 1;
+                            break;
+                        case SDLK_v:
+                            keyboard[0xF] = 1;
+                            break;
                     }
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym)
+                    {
+                        case SDLK_1:
+                            keyboard[0x1] = 0;
+                            break;
+                        case SDLK_2:
+                            keyboard[0x2] = 0;
+                            break;
+                        case SDLK_3:
+                            keyboard[0x3] = 0;
+                            break;
+                        case SDLK_4:
+                            keyboard[0xC] = 0;
+                            break;
+                        case SDLK_q:
+                            keyboard[0x4] = 0;
+                            break;
+                        case SDLK_w:
+                            keyboard[0x5] = 0;
+                            break;
+                        case SDLK_e:
+                            keyboard[0x6] = 0;
+                            break;
+                        case SDLK_r:
+                            keyboard[0xD] = 0;
+                            break;
+                        case SDKL_a:
+                            keyboard[0x7] = 0;
+                            break;
+                        case SDLK_s:
+                            keyboard[0x8] = 0;
+                            break;
+                        case SDLK_d:
+                            keyboard[0x9] = 0;
+                            break;
+                        case SDLK_f:
+                            keyboard[0xE] = 0;
+                            break;
+                        case SDLK_z:
+                            keyboard[0xA] = 0;
+                            break;
+                        case SDLK_x:
+                            keyboard[0x0] = 0;
+                            break;
+                        case SDLK_c:
+                            keyboard[0xB] = 0;
+                            break;
+                        case SDLK_v:
+                            keyboard[0xF] = 0;
+                            break;
+                    }
+                    break;
             }
         }
         execute();

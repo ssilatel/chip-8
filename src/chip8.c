01f4356b6346fb43486afedc9ddb3be838d8414d
ssilatel <setaxe25@gmail.com>
2023-08-30 15:36:54 +0200

    Implemented opcodes

--- src/chip8.c
+++ src/chip8.c
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
     screen = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);
 
     initChip8();
-    if (loadROM("roms/ibm-logo.ch8") != 0)
+    if (loadROM("roms/Cave.ch8") != 0)
     {
         cleanupSDL();
         return 1;
@@ -270,48 +270,163 @@ void execute()
     n = (opcode & 0x000F);
     kk = (opcode & 0x00FF);
     nnn = (opcode & 0x0FFF);
-    printf("Opcode: %x\n", opcode);
-    printf("Program Counter: %x\n", PC);
-    printf("I: %x\n", I);
+    // printf("Opcode: %x\n", opcode);
+    // printf("Program Counter: %x\n", PC);
+    // printf("I: %x\n", I);
 
     switch (opcode & 0xF000)
     {
         case 0x0000:
             switch (opcode & 0x00FF)
             {
+                // 00E0
                 case 0x00E0:
                     memset(display, 0, 64 * 32);
+                    drawFlag = 1;
+                    break;
+                // 00EE
+                case 0x00EE:
+                    sp--;
+                    PC = stack[sp];
+                    break;
+                default:
+                    printf("Error executing opcode %x\n", opcode);
                     break;
             }
             break;
+        // 1NNN
         case 0x1000:
             PC = nnn;
             break;
+        // 2NNN
         case 0x2000:
+            stack[sp] = PC;
+            sp++;
+            PC = nnn;
             break;
+        // 3XNN
         case 0x3000:
+            if (V[X] == kk)
+            {
+                PC += 2;
+            }
             break;
+        // $XNN
         case 0x4000:
+            if (V[X] != kk)
+            {
+                PC += 2;
+            }
             break;
+        // 5XY0
         case 0x5000:
+            if (V[X] == V[Y])
+            {
+                PC += 2;
+            }
             break;
+        // 6XNN
         case 0x6000:
             V[X] = kk;
             break;
+        // 7XNN
         case 0x7000:
             V[X] += kk;
             break;
         case 0x8000:
+            switch (n)
+            {
+                // 8XY0
+                case 0x0000:
+                    V[X] = V[Y];
+                    break;
+                // 8XY1
+                case 0x0001:
+                    V[X] |= V[Y];
+                    break;
+                // 8XY2
+                case 0x0002:
+                    V[X] &= V[Y];
+                    break;
+                // 8XY3
+                case 0x0003:
+                    V[X] ^= V[Y];
+                    break;
+                // 8XY4
+                case 0x0004:
+                    int tmp;
+                    tmp = (int)(V[X]) + (int)(V[Y]);
+                    if (tmp > 255)
+                    {
+                        V[0xF] = 1;
+                    }
+                    else
+                    {
+                        V[0xF] = 0;
+                    }
+                    V[X] += V[Y];
+                    // V[X] = tmp & 0xFF;
+                    break;
+                // 8XY5
+                case 0x0005:
+                    if (V[X] > V[Y])
+                    {
+                        V[0xF] = 1;
+                    }
+                    else
+                    {
+                        V[0xF] = 0;
+                    }
+                    V[X] -= V[Y];
+                    break;
+                // 8XY6
+                case 0x0006:
+                    // V[X] = V[Y];
+                    V[0xF] = V[X] & 1;
+                    V[X] >>= 1;
+                    break;
+                // 8XY7
+                case 0x0007:
+                    if (V[Y] > V[X])
+                    {
+                        V[0xF] = 1;
+                    }
+                    else
+                    {
+                        V[X] = 0;
+                    }
+                    V[X] = V[Y] - V[X];
+                    break;
+                // 8XYE
+                case 0x000E:
+                    // V[X] = V[Y];
+                    V[0xF] = V[X] >> 7;
+                    V[X] <<= 1;
+                    break;
+                default:
+                    printf("Error executing opcode %x\n", opcode);
+                    break;
+            }
             break;
+        // 9XY0
         case 0x9000:
+            if (V[X] != V[Y])
+            {
+                PC += 2;
+            }
             break;
+        // ANNN
         case 0xA000:
             I = nnn;
             break;
         case 0xB000:
+            PC = nnn + V[0x0];
             break;
+        // CXNN
         case 0xC000:
+            V[X] = (rand() % 100) & kk;
             break;
+        // DXYN
         case 0xD000:
             uint8_t x = V[X] % 64;
             uint8_t y = V[Y] % 32;
@@ -336,8 +451,98 @@ void execute()
             drawFlag = 1;
             break;
         case 0xE000:
+            switch (kk)
+            {
+                // EX9E
+                case 0x009E:
+                    if (keyboard[V[X]] != 0)
+                    {
+                        PC += 2;
+                    }
+                    break;
+                // EXA1
+                case 0x00A1:
+                    if (keyboard[V[X]] == 0)
+                    {
+                        PC += 2;
+                    }
+                    break;
+                default:
+                    printf("Error executing opcode %x\n", opcode);
+                    break;
+            }
             break;
         case 0xF000:
+            switch (kk)
+            {
+                // FX07
+                case 0x0007:
+                    V[X] = delayTimer;
+                    break;
+                // FX0A
+                case 0x000A:
+                    int keyPressed = 0;
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if (keyboard[i])
+                        {
+                            keyPressed = 1;
+                            V[X] = i;
+                        }
+                    }
+
+                    if (!keyPressed)
+                    {
+                        PC -= 2;
+                    }
+                    break;
+                // FX15
+                case 0x0015:
+                    delayTimer = V[X];
+                    break;
+                // FX18
+                case 0x0018:
+                    soundTimer = V[X];
+                    break;
+                // FX1E
+                case 0x001E:
+                    I += V[X];
+                    break;
+                // FX29
+                case 0x0029:
+                    I = V[X] * 5;
+                    break;
+                // FX33
+                case 0x0033:
+                    int vx;
+                    vx = V[X];
+                    memory[I] = (vx - (vx % 100)) / 100;
+                    vx -= memory[I] * 100;
+                    memory[I + 1] = (vx - (vx % 10)) / 10;
+                    vx -= memory[I + 1] * 10;
+                    memory[I + 2] = vx;
+                    break;
+                // FX55
+                case 0x0055:
+                    for (uint8_t i = 0; i <= X; i++)
+                    {
+                        memory[I + i] = V[i];
+                    }
+                    break;
+                // FX65
+                case 0x0065:
+                    for (uint8_t i = 0; i <= X; i++)
+                    {
+                        V[i] = memory[I + i];
+                        break;
+                    }
+                default:
+                    printf("Error executing opcode %x\n", opcode);
+                    break;
+            }
+            break;
+        default:
+            printf("Error executing opcode %x\n", opcode);
             break;
     }
 }
